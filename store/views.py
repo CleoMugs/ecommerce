a75cb834d12fd2ffedecb38fd98aa14d7338e780
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-03 21:41:53 +0300

    views logic

--- store/views.py
+++ store/views.py
@@ -1,36 +1,99 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+
 from .models import *
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 
 
 def store(request):
+
+	data = cartData(request)
+	cartItems = data['cartItems']
+	
 	products = Product.objects.all()
-	context = {'products': products}
+	context = {'products': products, 'cartItems':cartItems}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
 
-	else:
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
-
-	context = {'items':items, 'order':order}
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+		
+		
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
+
+	data = cartData(request)
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+
+	print('Action:', action)
+	print('productId:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+		
+	return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
+	
 
 	else:
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		customer, order = guestOrder(request, data)
 
-	context = {'items':items, 'order':order}
-	return render(request, 'store/checkout.html', context)
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == float(order.get_cart_total):
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+				customer=customer,
+				order=order,
+				address=data['shipping']['address'],
+				city=data['shipping']['city'],
+				state=data['shipping']['state'],
+				zipcode=data['shipping']['zipcode'],
+			)
+
+	return JsonResponse('Payment complete!', safe=False)
